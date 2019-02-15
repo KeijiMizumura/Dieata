c7106f6239c4dfd350a697a36e3a30ad2572cd15
MizumuraKeiji <mizumurakeiji09@gmail.com>
2019-02-15 13:43:02 -0800

    Database created.

--- app/src/main/java/tnt_codefest/dieta/Database/SQLiteHelper.java
+++ app/src/main/java/tnt_codefest/dieta/Database/SQLiteHelper.java
@@ -5,17 +5,47 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SQLiteHelper extends SQLiteOpenHelper {
-    public SQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    // Database Details
+    private static final String DATABASE_NAME = "dieata.db";
+    private static int DATABASE_VERSION = 1;
+
+    // User Table
+    private static final String USER_TABLE = "users";
+    private static final String USER_ID = "user_id";
+    private static final String USER_USERNAME = "user_name";
+    private static final String USER_FIRSTNAME = "first_name";
+    private static final String USER_LASTNAME = "last_name";
+    private static final String USER_HEIGHT = "height";
+    private static final String USER_WEIGHT = "weight";
+    private static final String USER_BMI = "bmi";
+    private static final String CURRENT_WEEK = "current_week";
+
+    // Create User table
+
+    private static final String CREATE_USER = "CREATE TABLE " + USER_TABLE + "(" +
+            USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            USER_USERNAME + " TEXT," +
+            USER_FIRSTNAME + " TEXT, " +
+            USER_LASTNAME + " TEXT, " +
+            USER_HEIGHT + " DOUBLE, " +
+            USER_WEIGHT + " DOUBLE, " +
+            USER_BMI + " DOUBLE, " +
+            CURRENT_WEEK + " INTEGER);";
+
+    public SQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_USER);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE);
 
+        onCreate(db);
     }
 }
